16fce22d55fe510084183469ecced150ec62eebd
wagneryhr-eng <wagner.yhr@gmail.com>
2026-03-28 08:11:41 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'dialup-cache-v2';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './sounds/dialup.mp3'
+];
+
+// インストール時にリソースをキャッシュ
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+            .then(() => self.skipWaiting())
+    );
+});
+
+// 古いキャッシュを削除
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+});
+
+// リクエスト発生時にキャッシュがあればそれを返し、なければネットワークへ
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
